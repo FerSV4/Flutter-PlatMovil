384cc15c25238e347dbb96dc3425d53fb9a53f98
FerSV4 <ferandressc@hotmail.com>
2026-04-23 14:35:49 -0400

    feature: header finalizado

--- PantallaUcb/replicaucb/lib/main.dart
+++ PantallaUcb/replicaucb/lib/main.dart
@@ -19,9 +19,32 @@ class MainApp extends StatelessWidget {
                   children: [
                     Container(
                       color: Color.fromARGB(255, 18, 41, 88),
+                      child: Text("UCB - Portal Academico"),
                     ), // titulo
-                    Container(), // Logo y datos
-                    Container() // Lista de materias
+                    Container(
+                      color: Color.fromARGB(255, 18, 41, 88),
+                      
+                      child: Row(
+                        children: [
+                          Icon(Icons.account_circle, size: 80, color: Colors.white), // TODO: importar logo de la U
+                          Column(
+                            children: [
+                              Text("Nombre del Estudiante", style: TextStyle(color: Colors.white),),
+                              Text("ALEJANDRO TORRES C.", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              Text('Carrera: Ingenieria de Software', style: TextStyle(color: Colors.white)),
+                              Text('7° Semestre | Registro 10456', style: TextStyle(color: Colors.white))
+                            ],
+                          )
+                        ],
+                      )
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        // TODO: Hacer la lista de materias
+                      ),
+                    ) // Lista de materias
                   ],
                 ),
               )
