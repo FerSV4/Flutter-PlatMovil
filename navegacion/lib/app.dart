6b82cea80010a726b027b94aa0206c4c7f3cdfb0
FerSV4 <ferandressc@hotmail.com>
2026-05-05 14:24:10 -0400

    Relocalizacion app Dart

---
+++ navegacion/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'pantalla_bienvenida.dart';
+import 'pantalla_catalogo.dart';
+import 'pantalla_perfil.dart';
+
+class AppAdopcion extends StatelessWidget {
+  const AppAdopcion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Refugio Tilines',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      initialRoute: PantallaBienvenida.ruta,
+      routes: {
+        PantallaBienvenida.ruta: (context) => const PantallaBienvenida(),
+        PantallaCatalogo.ruta: (context) => const PantallaCatalogo(),
+        PantallaPerfil.ruta: (context) => const PantallaPerfil(),
+      },
+    );
+  }
+}
--- navegacion/lib/main.dart
+++ navegacion/lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pantalla_bienvenida.dart';
-import 'pantalla_catalogo.dart';
-import 'pantalla_perfil.dart';
+import 'app.dart';
 
-void main() {
+void main() {  
   runApp(const AppAdopcion());
-}
-
-class AppAdopcion extends StatelessWidget {
-  const AppAdopcion({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Refugio Tilines',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-        useMaterial3: true,
-      ),
-      initialRoute: PantallaBienvenida.ruta,
-      routes: {
-        PantallaBienvenida.ruta: (context) => const PantallaBienvenida(),
-        PantallaCatalogo.ruta: (context) => const PantallaCatalogo(),
-        PantallaPerfil.ruta: (context) => const PantallaPerfil(),
-      },
-    );
-  }
 }
