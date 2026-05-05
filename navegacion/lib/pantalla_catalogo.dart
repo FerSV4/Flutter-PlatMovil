d94017a5736c691f5faa32f84b7f191985113429
FerSV4 <ferandressc@hotmail.com>
2026-05-04 23:10:55 -0400

    fix: optimizar el uso del build

--- navegacion/lib/pantalla_catalogo.dart
+++ navegacion/lib/pantalla_catalogo.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'pantalla_perfil.dart';
+import 'mascota.dart';
 
 class PantallaCatalogo extends StatelessWidget {
+  static const String ruta = '/catalogo';
+
+  final List<Mascota> listaMascotas = const [
+    Mascota(nombre: 'Ñiky', tipo: 'Perro', edad: '2 años', color: 'Caramelo'),
+    Mascota(nombre: 'Yan', tipo: 'Gato', edad: '1 mes', color: 'Negro'),
+    Mascota(nombre: 'Pol', tipo: 'Perro beniano', edad: '3 años', color: 'Verde'),
+    Mascota(nombre: 'TropiFrut', tipo: 'Tatu', edad: '6 meses', color: 'Cafe y negro'),
+  ];
+
   const PantallaCatalogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final listaMascotas = [
-      {'nombre': 'Ñiky', 'tipo': 'Perro', 'edad': '2 años', 'color': 'Caramelo'},
-      {'nombre': 'Yan', 'tipo': 'Gato', 'edad': '1 mes', 'color': 'Negro'},
-      {'nombre': 'Pol', 'tipo': 'Perro beniano', 'edad': '3 años', 'color': 'Verde'},
-      {'nombre': 'TropiFrut', 'tipo': 'Tatu', 'edad': '6 meses', 'color': 'Cafe y negro'},
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mascotas firmes:'),
@@ -25,17 +29,25 @@ class PantallaCatalogo extends StatelessWidget {
             margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
             child: ListTile(
               leading: const CircleAvatar(
-                backgroundColor: Colors.orangeAccent
+                backgroundColor: Colors.orangeAccent,
               ),
-              title: Text(mascota['nombre']!),
-              subtitle: Text(mascota['tipo']!),
+              title: Text(mascota.nombre),
+              subtitle: Text(mascota.tipo),
               trailing: const Icon(Icons.arrow_forward),
-              onTap: () {
-                Navigator.pushNamed(
+              onTap: () async {
+                final resultado = await Navigator.pushNamed(
                   context,
-                  '/perfil',
+                  PantallaPerfil.ruta,
                   arguments: mascota,
                 );
+
+                if (!context.mounted) return;
+
+                if (resultado != null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Anterior Pantalla: $resultado')),
+                  );
+                }
               },
             ),
           );
